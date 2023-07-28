95ccabc163c90c1b114087b06ce4f9a03d1fa54e
farid <faridramy2003@gmail.com>
2023-07-28 19:55:34 +0300

    finish json text area v1

--- src/app/json-text-area/json-text-area.component.ts
+++ src/app/json-text-area/json-text-area.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ace from 'ace-builds';
 
 @Component({
   selector: 'app-json-text-area',
   templateUrl: './json-text-area.component.html',
-  styleUrls: ['./json-text-area.component.scss']
+  styleUrls: ['./json-text-area.component.scss'],
 })
 export class JsonTextAreaComponent {
+  json_object = `test`;
 
+  editor!: ace.Ace.Editor;
+
+  ngOnInit() {
+    this.initAceEditor();
+  }
+
+  initAceEditor() {
+    this.editor = ace.edit('editor');
+
+    // => Set editor options
+    this.editor.setTheme('ace/theme/monokai'); // Set theme
+    this.editor.getSession().setMode('ace/mode/javascript'); // Set language 
+    this.editor.setValue(this.json_object); // Set text 
+  }
 }
