30af5cfc04102f010ee89ac8e140589c5da74784
farid <faridramy2003@gmail.com>
2023-07-28 19:17:55 +0300

    added the initial files for the json text area

---
+++ src/app/json-text-area/json-text-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-json-text-area',
+  templateUrl: './json-text-area.component.html',
+  styleUrls: ['./json-text-area.component.scss']
+})
+export class JsonTextAreaComponent {
+
+}
